ba579ddefe08ebc3245112e2beb7d49a1386b459
steskelt <steven.skelton@publicissapient.com>
2024-02-12 13:53:47 -0500

    exec tests

--- src/main/scala/ca/stevenskelton/httpmavenreceiver/Main.scala
+++ src/main/scala/ca/stevenskelton/httpmavenreceiver/Main.scala
@@ -10,6 +10,8 @@ import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.{HttpApp, HttpRoutes}
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 
+import java.io.File
+
 object Main extends /*epollcat.EpollApp */ IOApp {
 
   case class ExitException(msg: String, code: ExitCode = ExitCode.Error) extends Exception(msg)
@@ -22,42 +24,42 @@ object Main extends /*epollcat.EpollApp */ IOApp {
     case request@PUT -> Root / "releases" => handler.releasesPut(request)
   }.orNotFound
 
-  def jarDirectory = new java.io.File(getClass.getProtectionDomain.getCodeSource.getLocation.toURI.toString)
+  def jarDirectory: File = new java.io.File(getClass.getProtectionDomain.getCodeSource.getLocation.toURI.toString).getParentFile
 
   override def run(args: List[String]): IO[ExitCode] = {
     logger.info(s"${SbtBuildInfo.name} ${SbtBuildInfo.version}") *>
       MainArgs.parse(args, jarDirectory)
-      .flatMap {
-        mainArgs =>
+        .flatMap {
+          mainArgs =>
 
-          given httpClient: Resource[IO, Client[IO]] = EmberClientBuilder
-            .default[IO]
-            .withLogger(logger)
-            .build
+            given httpClient: Resource[IO, Client[IO]] = EmberClientBuilder
+              .default[IO]
+              .withLogger(logger)
+              .build
 
-          val handler = RequestHandler(
-            mainArgs.uploadDirectory,
-            mainArgs.allowAllVersions,
-            mainArgs.disableMaven,
-            mainArgs.postUploadAction,
-          )
+            val handler = RequestHandler(
+              mainArgs.uploadDirectory,
+              mainArgs.allowAllVersions,
+              mainArgs.disableMaven,
+              mainArgs.postUploadAction,
+            )
 
-          EmberServerBuilder
-            .default[IO]
-            .withReceiveBufferSize(mainArgs.maxUploadByteSize)
-            //      .withHttp2
-            .withHost(mainArgs.host)
-            .withPort(mainArgs.port)
-            .withHttpApp(httpApp(handler))
-            .withLogger(logger)
-            .build
-            .useForever
-            .as(ExitCode.Success)
-      }
-      .handleErrorWith {
-        case ExitException(msg, code) => logger.error(msg).as(code)
-        case ex => logger.error(ex)(ex.getMessage).as(ExitCode.Error)
-      }
+            EmberServerBuilder
+              .default[IO]
+              .withReceiveBufferSize(mainArgs.maxUploadByteSize)
+              //      .withHttp2
+              .withHost(mainArgs.host)
+              .withPort(mainArgs.port)
+              .withHttpApp(httpApp(handler))
+              .withLogger(logger)
+              .build
+              .useForever
+              .as(ExitCode.Success)
+        }
+        .handleErrorWith {
+          case ExitException(msg, code) => logger.error(msg).as(code)
+          case ex => logger.error(ex)(ex.getMessage).as(ExitCode.Error)
+        }
 
   }
 }
